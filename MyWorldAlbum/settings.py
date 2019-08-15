99dfc93c747effda555c7f63fccd0cdd2f37f6ec
audelcroix <au.delcroix@live.fr>
2019-08-15 16:24:42 +0200

    update 2

--- MyWorldAlbum/settings.py
+++ MyWorldAlbum/settings.py
@@ -21,10 +21,10 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = '+_gy9)bd%#h158%lqar-vxzh7o6k$c+i!x23it@uhqd_4q97i-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
--- album/static/js/album/ajax.js
+++ album/static/js/album/ajax.js
@@ -23,7 +23,10 @@ $(document).ready(function() {
                 $("#comment-zone").prepend("<div class='comment-area bg-grey' data-commentId="+json.newCommentId+"> <div class='row no-gutters'> <div class='col-md-2 col-sm-0 profile-pic-area'></div> <div class='col-md-10 col-sm-12 description-area'></div> </div> </div>");
                 $("#comment-zone div.profile-pic-area:first").append("<img src='/media/"+json.userImg+"' class='commentProfilePic' alt='"+json.username+"'>");
                 $("#comment-zone div.description-area:first").append("<h6 class='pt-1 color-green'>By "+json.username+" <a class='text-danger commentDeleteTrigger float-right mr-2 mt-1'><i class='far fa-trash-alt fa-lg'></i></a></h6><p class='color-white'>"+comment_content+"</p>");
-                $("#comment-zone div.comment-area:last").remove();
+
+                if ($("#comment-zone").children(".comment-area").length > 5) {
+                  $("#comment-zone div.comment-area:last").remove();
+                }
                 $("#commentFormTextarea").val("");
               } else {
                 alert("An error occured");
@@ -31,8 +34,8 @@ $(document).ready(function() {
           },
 
           error : function(xhr,errmsg,err) {
-              $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: "+errmsg+
-                  " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
+            $('.error-zone').css("display", "block");
+            $('.error-target').val('Sorry, an error occured');
           }
       });
   }
